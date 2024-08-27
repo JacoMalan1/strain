54b9349de13ad05b3a62f7191914b0c1d6734620
Jaco Malan <jacomalan.12@gmail.com>
2024-08-28 00:23:05 +0200

    Added nix dev shell

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "NixOS dev shell flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      rustVersion = pkgs.rust-bin.stable.latest.default;
+
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = rustVersion;
+        rustc = rustVersion;
+      };
+
+      rustBuild = rustPlatform.buildRustPackage {
+        pname = "strain";
+        version = "0.1.2";
+        src = ./.;
+
+        buildInputs = with pkgs; [
+          gmp
+          mpfr
+          libmpc
+        ];
+      
+        env = {
+          CFLAGS = ''
+            -I${pkgs.gmp.dev}/include 
+            -L${pkgs.gmp}/lib 
+            -I${pkgs.mpfr.dev}/include 
+            -L${pkgs.mpfr}/lib
+            -I${pkgs.libmpc}/include
+            -L${pkgs.libmpc}/lib
+          '';
+        };
+
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+    in {
+      defaultPackage = rustBuild;
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          gmp
+          mpfr
+          libmpc
+        ];
+
+        env = {
+          CFLAGS = ''
+            -I${pkgs.gmp.dev}/include 
+            -L${pkgs.gmp}/lib 
+            -I${pkgs.mpfr.dev}/include 
+            -L${pkgs.mpfr}/lib
+            -I${pkgs.libmpc}/include
+            -L${pkgs.libmpc}/lib
+          '';
+        };
+      };
+    });
+}
